2b1c1a97e3d3d79ef68917c06ec343b12c289aff
Roseau <asboyo@gmail.com>
2018-01-05 17:19:24 +0700

    ubah dengan switch case

--- main/Utama.java
+++ main/Utama.java
@@ -11,6 +11,7 @@ public class Utama{
         Persegi p = new Persegi();
         PersegiPanjang pp = new PersegiPanjang();
         Scanner read = new Scanner(System.in);
+        /*
         System.out.println("====Lingkaran====");
         System.out.print("Masukkan jari-jari lingkaran : ");
         int a = read.nextInt();
@@ -25,5 +26,31 @@ public class Utama{
         System.out.print("Masukkan lebar : ");
         int d = read.nextInt();
         pp.tampilLuas(c,d);
+        */
+        System.out.println("===============================");
+        System.out.println("1. Lingkaran");
+        System.out.println("2. Persegi");
+        System.out.println("3. Persegi panjang");
+        System.out.print("pilih bentuk yang anda inginkan : ");
+        int pilih = read.nextInt();
+        switch(pilih){
+            case 1 :
+                System.out.print("Masukkan nilai jari - jari : ");
+                int a = read.nextInt();
+                l.tampilLuas(a);
+            break;
+            case 2 :
+                System.out.print("Masukkan nilai sisi : ");
+                int b = read.nextInt();
+                p.tampilLuas(b);
+            break;
+            case 3 :
+                System.out.print("Masukkan nilai panjang : ");
+                int c = read.nextInt();
+                System.out.print("Masukkan nilai lebar : ");
+                int d = read.nextInt();
+                pp.tampilLuas(c,d);
+            break;
+        }
     }
 }
